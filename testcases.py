dc7ed9395e071ee36164d7772c1f3cf7ee1cefb8
IreniR <48959396+IreniR@users.noreply.github.com>
2020-03-07 22:10:28 -0500

    function1 test

--- testcases.py
+++ testcases.py
@@ -1,1 +1,11 @@
+class TestLoadWordsTrue(TestCase):
+    def test_loadWords(self):
+        expected = 'a'
+        actual = 'a'
+        self.assertEqual(expected, actual, "The word exists")
 
+class TestLoadWordsFalse(TestCase):
+    def test_loadWords(self):
+        expected = 'a'
+        actual = 'b'
+        self.assertEqual(expected, actual, "The does not exists")

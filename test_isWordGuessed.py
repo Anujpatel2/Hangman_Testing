2d6a166d2ac8cd9d326483a2721b7b63292abae6
biv147 <vraj319@gmail.com>
2020-03-08 01:21:08 -0500

    Fixed the function testcases

--- test_isWordGuessed.py
+++ test_isWordGuessed.py
@@ -1,18 +1,19 @@
+import hangman
 from unittest import TestCase
 
 class TestWordGuessedCorrect(TestCase):
+
     def test_isWordGuessed(self):
-        actual = 'true'
-        expected = 'true'
-        self.assertEqual(actual, expected, "The word you guessed it correct")
+        """
+        This test case function checks if the user has guessed the
+        word correctly
+        """
+        actual = hangman.isWordGuessed("boss", "boss")
+        self.assertTrue(actual, "The word you guessed it correct")
 
-class TestWordGuessedNotCorrect(TestCase):
-    def test_isWordGuessed(self):
-        actual = 'false'
-        expected = 'false'
-        self.assertNotEqual(actual, expected, "The word you guessed isn't correct")
-
-
-
-
-
+    def test_isWordGuessed2(self):
+        """
+        This test case fucntion checks if the user has guessed thw word wrong
+        """
+        actual = hangman.isWordGuessed("boss", "boot")
+        self.assertFalse(actual, "The word you guessed is not correct")
